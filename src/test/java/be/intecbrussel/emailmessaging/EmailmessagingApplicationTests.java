a8bc2d1a7135c4717336ef0dee7195c4955c6c14
NadiraJan <nadira.janbaeva@hotmail.com>
2022-11-22 11:37:54 +0100

    Initial commit

---
+++ src/test/java/be/intecbrussel/emailmessaging/EmailmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.intecbrussel.emailmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
